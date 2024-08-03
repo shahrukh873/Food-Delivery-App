457da8df26eef8d4e0a5db3af8a3ea6cb3252338
shahrukh873 <113853744+shahrukh873@users.noreply.github.com>
2024-08-03 19:41:37 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const currency = '₹'
 export const assets ={
     logo,
     add_icon,
